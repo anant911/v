4d10f5945187b8b3d3db1d311e77ebc6a097e8e6
anant911 <77596400+anant911@users.noreply.github.com>
2022-04-07 18:32:45 -0400

    Add files via upload

---
+++ bathroom.js
@@ -0,0 +1,34 @@
+img="";
+status="";
+
+function preload()
+{
+img=loadImage("bathroom.jpeg");
+}
+
+function setup()
+{
+    canvas=createCanvas(600,400);
+    canvas.center();
+objectDetectore=ml5.objectDetector("cocossd", modelLoaded);
+document.getElementById("status").innerHTML="status: detecting objects";
+}
+
+function draw()
+{
+image(img,0,0,600,400);
+}
+function modelLoaded()
+{
+    console.log("model loaded");
+    status=true;
+    objectDetector.detect(img, gotResults);
+}
+s
+function gotResults(error, results)
+{
+if(error){
+    console.error(error);
+}
+console.log(results);
+}
---
+++ bedroom.js
@@ -0,0 +1,35 @@
+img="";
+status="";
+
+function preload()
+{
+img=loadImage("bedroom.png");
+}
+
+function setup()
+{
+    canvas=createCanvas(600,400);
+    canvas.center();
+   
+objectDetectore=ml5.objectDetector("cocossd", modelLoaded);
+document.getElementById("status").innerHTML="status: detecting objects";
+}
+
+function draw()
+{
+image(img,0,0,600,400);
+}
+function modelLoaded()
+{
+    console.log("model loaded");
+    status=true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+if(error){
+    console.error(error);
+}
+console.log(results);
+}
